d22c2bb18ee02e80004b4ab850d334e7b922070d
Alfonso <alfof87@gmail.com>
2020-09-02 15:01:06 +0200

    php-snacks-blocco-1

--- index.php
+++ index.php
@@ -4,14 +4,22 @@ Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squ
 punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema. -->
 <?php
  $arr = [
-   $partita1 = ["Olimpia Milano", "Virtus Bologna", "55-60"],
-   $partita2 = ["Cantù", "Fortitudo Bologna", "55-15"],
-   $partita3 = ["Reyer", "Pallacanestro Varese", "30-30"],
-   $partita4 = ["Dinamo", "Pallacanestro Brescia", "60-45"]
+   $partita1 = ["Olimpia Milano", "-Virtus Bologna", " 55-60"],
+   $partita2 = ["Cantù", "-Fortitudo Bologna", " 55-15"],
+   $partita3 = ["Reyer", "-Pallacanestro Varese", " 30-30"],
+   $partita4 = ["Dinamo", "-Pallacanestro Brescia", " 60-45"]
  ];
-
+ // foreach ($arr as $elem) {
+ //   echo $elem;
+ // }
+ echo count($arr);
+ echo "<br>";
  for ($i=0; $i < count($arr); $i++) {
    $elem = $arr[$i];
-   echo array_keys($arr);
+   echo $elem[$i];
+   // $elem = $arr[$i];
+   // $elem2 = $elem[$i];
+   // $elem3 = $elem2[i];
+   // echo ($elem3);
  }
 ?>
