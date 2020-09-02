0575735981430cea6be2f4359a1be02557131fdb
Alfonso <alfof87@gmail.com>
2020-09-02 15:40:54 +0200

    php-snacks-blocco-1

--- index.php
+++ index.php
@@ -4,16 +4,20 @@ Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squ
 punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema. -->
 <?php
  $arr = [
-   $partita1 = ["Olimpia Milano", "-Virtus Bologna", " 55-60"],
-   $partita2 = ["Cantù", "-Fortitudo Bologna", " 55-15"],
-   $partita3 = ["Reyer", "-Pallacanestro Varese", " 30-30"],
-   $partita4 = ["Dinamo", "-Pallacanestro Brescia", " 60-45"]
+   ["Olimpia Milano", "Virtus Bologna", " 55-60"],
+   ["Cantù", "Fortitudo Bologna", " 55-15"],
+   ["Reyer", "Pallacanestro Varese", " 30-30"],
+   ["Dinamo", "Pallacanestro Brescia", " 60-45"]
  ];
 
- echo count($arr);
- echo "<br>";
  for ($i=0; $i < count($arr); $i++) {
-   $elem = $arr[$i];
-   echo $elem[$i];
+   $elem = $arr[$i][0] . " " . "vS" . " " . $arr[$i][1] . " ---> " . "risultato:" . $arr[$i][2];
+   echo $elem;
+   echo "<br>";
  }
 ?>
+<!-- Snack 2
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione)
+che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero.
+Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato"
+ -->
